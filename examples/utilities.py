9ae435762034eae8a97be15ba3cd0a0d915e08f5
Sandbergo <larslsa@stud.ntnu.no>
2021-02-28 18:42:12 +0100

    instance generator developed

--- examples/utilities.py
+++ examples/utilities.py
@@ -5,12 +5,12 @@ class Logger:
     def __init__(self, filename=None):
         self.logfile = filename
 
-    def format_time():
+    def format_time(self):
         t = datetime.datetime.now()
         s = t.strftime('%Y-%m-%d %H:%M:%S.%f')
         return s[:-4]
 
-    def log(self, str: str):
+    def __call__(self, str: str):
         str = f'[{self.format_time()}] {str}'
         print(str)
         if self.logfile is not None:
